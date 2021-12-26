d9b8e70261e203296d5b6cc3478ce0b2e5cdc341
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-12-27 00:56:37 +0600

    Class eleven - End of JS

---
+++ eleven.js
@@ -0,0 +1,57 @@
+console.log("Eleven");
+
+/**
+ * Short Syllabus
+1.	Data Structure:
+a.	Linked list
+b.	Double linked list
+c.	Binary search tree
+d.	Stack
+e.	Queue
+
+2.	Algorithm:
+a.	Time complexity
+b.	Space complexity
+c.	Selection sort (Time complexity & space complexity)
+d.	Insertion sort (Time complexity & space complexity)
+e.	Bubble sort (Time complexity & space complexity)
+f.	Merge sort (Time complexity & space complexity)
+g.	Quick sort (Time complexity & space complexity)
+
+3.	Database
+a.	Er diagram
+b.	Various keys(foreign key,primary,unique,candidate,composite key)
+c.	Relation:
+i.	One to one
+ii.	One to many
+iii.	Many to one
+iv.	Many to many
+d.	Join:
+i.	Left inner join
+ii.	Right inner join
+iii.	Cross join
+iv.	Outer join
+
+e.	Transaction
+f.	Trigger
+g.	Function
+h.	Indexing
+
+4.	OOP
+a.	Encapsulation
+b.	Inheritance 
+c.	polymorphism
+d.	Compile time polymorphism vs run time polymorphism
+e.	abstraction
+f.	Abstract class
+g.	Access modifier
+h.	Multiple inheritance vs multilevel inheritance
+i.	Interface
+j.	Overloading vs overriding
+
+5.	Networking
+a.	Network  7 layer
+b.	Tcp vs udp
+c.	Application of tcp vs udp
+
+ */
